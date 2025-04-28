16baf3b4faae349be4079905a8d774badafae681
TomiHuspenina <thuspenina@gmail.com>
2025-04-28 15:20:30 -0300

    http added

--- helm-api/main.go
+++ helm-api/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"helm-api/model"
 	"helm-api/service"
+	"net/http"
 )
 
 func main() {
-	req := model.HelmRequest{
-		ChartURL: "https://charts.bitnami.com/bitnami/nginx-15.0.0.tgz",
+	http.HandleFunc("/get-images", getImagesHandler)
+
+	fmt.Println("Servidor corriendo en :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getImagesHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req model.HelmRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || req.ChartURL == "" {
+		http.Error(w, fmt.Sprintf("error decoding json", err), http.StatusBadRequest)
+		return
 	}
 
 	images, err := service.GetImagesFromChartURL(req)
 	if err != nil {
-		fmt.Println("Error:", err)
+		http.Error(w, fmt.Sprintf("error getting images from url", err), http.StatusInternalServerError)
 		return
 	}
 
-	for _, img := range images {
-		fmt.Printf("Image: %s, Size: %s, Layers: %d\n", img.Image, img.Size, img.NumLayers)
-	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(images)
 }
