63809cdfdfedf2159de9aadf8e0b42afc57fa0e3
TomiHuspenina <thuspenina@gmail.com>
2025-04-28 22:20:58 -0300

    gin-gonic

--- helm-api/main.go
+++ helm-api/main.go
@@ -1,43 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"helm-api/model"
 	"helm-api/service"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	router *gin.Engine
 )
 
 func main() {
-	http.HandleFunc("/get-images", getImagesHandler)
+	router = gin.Default()
+	router.POST("/images", getImagesHandler)
+	router.Run(":8080")
 
 	fmt.Println("Servidor corriendo en :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+
 }
 
-func getImagesHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		return
-	}
+func getImagesHandler(c *gin.Context) {
 
 	var req model.HelmRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
+	err := c.ShouldBindJSON(&req)
 	if err != nil || req.ChartURL == "" {
-		http.Error(w, fmt.Sprintf("error decoding json", err), http.StatusBadRequest)
+		c.JSON(http.StatusBadGateway, gin.H{"error": "invalid request body"})
 		return
 	}
 
 	images, err := service.GetImagesFromChartURL(req)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("error getting images from url", err), http.StatusInternalServerError)
-		return
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": ""})
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(images)
+	c.JSON(http.StatusOK, images)
 }
