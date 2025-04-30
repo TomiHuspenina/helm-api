3c1fc211b0dad5b846f1672ea8e78f2ce3db281b
TomiHuspenina <thuspenina@gmail.com>
2025-04-30 00:16:15 -0300

    changes

--- helm-api/service/helm_service.go
+++ helm-api/service/helm_service.go
@@ -14,10 +14,18 @@ import (
 	"helm.sh/helm/v3/pkg/getter"
 )
 
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/mysql-10.1.0.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/apache-10.0.4.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/nginx-10.0.4.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/redis-17.0.0.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/mariadb-12.0.0.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/postgresql-12.0.0.tgz
+//http://localhost:8080/?url=https://charts.bitnami.com/bitnami/wordpress-17.0.0.tgz
+
 func GetImagesFromChartURL(url string) ([]model.ImageInfo, apierrors.ApiError) {
 
 	var settings = cli.New()
-	chartDwldr := downloader.ChartDownloader{
+	chartDwldr := downloader.ChartDownloader{ //descarga el chart (.tgz)
 		Out:     os.Stdout,
 		Getters: getter.All(settings),
 		Options: []getter.Option{},
@@ -29,17 +37,17 @@ func GetImagesFromChartURL(url string) ([]model.ImageInfo, apierrors.ApiError) {
 		return nil, apierrors.NewInternalServerApiError("error downloading chart", err)
 	}
 
-	chartReq, err := loader.Load(file) //load the file
+	chartReq, err := loader.Load(file) //lee charthelm del archivo descargado para crear la estructura
 	if err != nil {
 		return nil, apierrors.NewInternalServerApiError("error loading chart", err)
 	}
 
-	actionConfig := new(action.Configuration)
+	actionConfig := new(action.Configuration) //entorno de instalacion falso
 	err = actionConfig.Init(settings.RESTClientGetter(), "default", "memory", func(format string, v ...interface{}) {})
 	if err != nil {
 		return nil, apierrors.NewInternalServerApiError("error initializing action config", err)
 	}
-	install := action.NewInstall(actionConfig)
+	install := action.NewInstall(actionConfig) //instalador de chart helm
 	install.DryRun = true
 	install.ClientOnly = true
 	install.ReleaseName = "dummy"
@@ -47,14 +55,14 @@ func GetImagesFromChartURL(url string) ([]model.ImageInfo, apierrors.ApiError) {
 
 	values := chartReq.Values
 
-	release, err := install.Run(chartReq, values)
+	release, err := install.Run(chartReq, values) //genera el contenido yaml del chart
 	if err != nil {
 		return nil, apierrors.NewInternalServerApiError("error running install dry-run", err)
 	}
 
 	var images []model.ImageInfo
 
-	lines := strings.Split(release.Manifest, "\n")
+	lines := strings.Split(release.Manifest, "\n") //separa el contenido en lineas
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if strings.HasPrefix(line, "image:") {
@@ -90,14 +98,14 @@ func getImagesDetail(imageRef string) (int64, int, error) {
 		return 0, 0, err
 	}
 
-	layers, err := img.Layers()
+	layers, err := img.Layers() //extrae las capas de la imagen
 	if err != nil {
-		return 0, 0, nil
+		return 0, 0, err
 	}
 
 	var totalSize int64 = 0
 	for _, layer := range layers {
-		size, err := layer.Size()
+		size, err := layer.Size() //obtiene el tamaño de cada capa
 		if err != nil {
 			return 0, 0, err
 		}
